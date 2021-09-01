b7eddc7b7a756e47fd6c0c27d0afbbb7fe703fca
Ben Kostiuk <kostiukb@mcmaster.ca>
2021-09-01 13:25:00 -0400

    Delete UnsupportedVoteActionException.java

--- src/main/java/com/unityTest/courseManagement/exception/UnsupportedVoteActionException.java
+++
@@ -1,7 +0,0 @@
-package com.unityTest.courseManagement.exception;
-
-public class UnsupportedVoteActionException extends RuntimeException {
-	public UnsupportedVoteActionException(String action) {
-		super(String.format("Unsupported vote action %s", action));
-	}
-}
